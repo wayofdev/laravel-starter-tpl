33961d2ac7efd2fbff901c3e0413d3ad1c5db50c
lotyp <lotyp7@gmail.com>
2023-05-11 11:30:06 +0300

    fix: change how app is initialized

---
+++ app/src/Bridge/Laravel/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Laravel;
+
+use Illuminate\Foundation\Application as LaravelApplication;
+
+class Application extends LaravelApplication
+{
+    protected $namespace = 'Laravel\\';
+
+    public function path($path = ''): string
+    {
+        return $this->basePath . DIRECTORY_SEPARATOR . 'src/Bridge/Laravel' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+}
