c0db28a069fc685d78c71aac13c5f0726d7c6d66
lotyp <lotyp7@gmail.com>
2024-06-13 16:26:40 +0300

    style: apply cs-fixer

--- app/src/Domain/Category/Models/Category.php
+++ app/src/Domain/Category/Models/Category.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Domain\Category\Models;
 
-use Database\Factories\CategoryFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class Category extends Model
--- app/src/Domain/Product/Models/Product.php
+++ app/src/Domain/Product/Models/Product.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Domain\Product\Models;
 
-use Database\Factories\ProductFactory;
 use Domain\Category\Models\Category;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
